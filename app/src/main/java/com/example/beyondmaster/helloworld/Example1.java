a5ec993209930574049187b51c502772acb0c05b
BeyondMaster <aza@gmx.ch>
2018-09-22 21:14:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/beyondmaster/helloworld/Example1.java
@@ -0,0 +1,13 @@
+package com.example.beyondmaster.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Example1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example1);
+    }
+}
